d381ee2bbaeca41f382eac94e577f7d2c40ef24e
s4l3h1 <s4l3h1@chmail.ir>
2017-06-05 02:57:41 +0430

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+error_msg(){
+      echo "There is no environment variable to config mysql for barnyard writes" > /dev/stderr
+      rm /etc/snort/config.lock
+}
+
+if [ -e /etc/snort/configured ]; then
+	echo "Barnyard Configuration has been compeleted before this time..."
+  barnyard2 -c /etc/snort/barnyard2.conf -d /var/log/snort -f snort.u2 -w /var/log/snort/barnyard2.waldo -g snort -u snort -D
+	exit 0
+fi
+
+if [ ! -e /etc/snort/config.lock ]; then
+   touch /etc/snort/config.lock
+   if [ -z $mysql_host ]; then
+	echo "MySQL Host Address is undefined" > /dev/stderr
+	error_msg
+   elif [ -z $mysql_user ]; then
+	echo "MySQL Username is undefined" > /dev/stderr
+	error_msg
+   elif [ -z $mysql_password ]; then
+	echo "MySQL password is undefined" > /dev/stderr
+	error_msg
+   elif [ -z $mysql_db ]; then
+	echo "MySQL DatabaseName is undefined" > /dev/stderr
+	error_msg
+   elif [ -z $sensor_name ]; then
+	echo "Sensor Name is undefined" > /dev/stderr
+	error_msg
+   else
+        cd /var/www/html/snorby
+        sed -e 's/username.*/username: $mysql_user/' -e 's/password.*/password: $mysql_password/' -e 's/host.*/host: $mysql_host/' config/database.yml
+        RAILS_ENV=production bundle exec rake snorby:setup
+        mysql --host=$mysql_host -u$mysql_user -p$mysql_password $mysql_db -e "source /opt/snort_src/barnyard2-master/schemas/create_mysql;"
+        echo "output database: log, mysql, user=$mysql_user password=$mysql_password dbname=$mysql_db host=$mysql_host sensor name=$sensor_name" | tee -a /etc/snort/barnyard2.conf
+	touch /etc/snort/configured 
+        exit 0
+   fi
+fi
+
+exit 1
